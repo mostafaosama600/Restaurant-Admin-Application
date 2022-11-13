22125ef20c992b55f1699f83f0049f86bfbac1b5
mostafa osama <mostafaelshrombably775@gmail.com>
2022-11-14 00:38:04 +0200

    set up foddie system

---
+++ src/App.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <router-view />
+  </div>
+</template>
+
+<style lang="scss">
+#app {
+  text-align: center;
+  color: #282828;
+}
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="welcom-msg">
+    <h1>Hello foddie system</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "HelloWorld",
+};
+</script>
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.welcom-msg {
+  h1 {
+    color: #282828;
+  }
+}
+</style>
